59f4785e21f4e6373e7ef3de11729bc0692800bf
Nick M <mully7773@gmail.com>
2022-11-30 14:14:52 -0600

    Add dummy data books along with uuid for unique ids

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import TestComp from "./components/TestComp";
 
 function App() {
+  const books = [
+    {
+      id: uuidv4(),
+      bookTitle: "Great Expectations",
+      bookAuthor: "Charles Dickens",
+      pages: 544,
+      dateCompleted: new Date(2020, 7, 14),
+    },
+    {
+      id: uuidv4(),
+      bookTitle: "Anna Karenina",
+      bookAuthor: "Leo Tolstoy",
+      pages: 864,
+      dateCompleted: new Date(2019, 9, 25),
+    },
+    {
+      id: uuidv4(),
+      bookTitle: "Tom Sawyer",
+      bookAuthor: "Mark Twain",
+      pages: 216,
+      dateCompleted: new Date(2016, 4, 24),
+    },
+    {
+      id: uuidv4(),
+      bookTitle: "Dracula",
+      bookAuthor: "Bram Stoker",
+      pages: 418,
+      dateCompleted: new Date(2015, 8, 2),
+    },
+  ];
+  console.log(books);
   return (
     <div>
       <TestComp />
