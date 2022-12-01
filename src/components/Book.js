2d94c5069ec2f3f703a729bf2099011c3087670f
Nick M <mully7773@gmail.com>
2022-11-30 23:11:06 -0600

    Add delete button to books and apply animation styles to books

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,19 +5,22 @@ import "./Book.scss";
 const Book = (props) => {
   console.log(props);
   const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const day = props.date.toLocaleString("en-US", { day: "numeric" });
   const year = props.date.getFullYear();
   return (
     <Card>
       <div className="book-description">
         <h2 className="book-description__book-title">{props.title}</h2>
         <br></br>
-        <span>by</span>
+        <span className="book-description__by-text">by</span>
         <p className="book-description__book-author">{props.author}</p>
         <p className="book-description__book-pages">{props.pages} Pages</p>
         <p className="book-description__date">
           {month} {day}, {year}
         </p>
+        <div className="x-container">
+          <button className="x-container__button">&times;</button>
+        </div>
       </div>
     </Card>
   );
--- src/components/UI/BookView.js
+++ src/components/UI/BookView.js
@@ -4,31 +4,34 @@ import "./BookView.scss";
 const BookView = (props) => {
   console.log(props);
   return (
-    <div className="book-view">
-      <Book
-        title={props.books[0].bookTitle}
-        author={props.books[0].bookAuthor}
-        pages={props.books[0].pages}
-        date={props.books[0].dateCompleted}
-      />
-      <Book
-        title={props.books[1].bookTitle}
-        author={props.books[1].bookAuthor}
-        pages={props.books[1].pages}
-        date={props.books[1].dateCompleted}
-      />
-      <Book
-        title={props.books[2].bookTitle}
-        author={props.books[2].bookAuthor}
-        pages={props.books[2].pages}
-        date={props.books[2].dateCompleted}
-      />
-      <Book
-        title={props.books[3].bookTitle}
-        author={props.books[3].bookAuthor}
-        pages={props.books[3].pages}
-        date={props.books[3].dateCompleted}
-      />
+    <div className="book-view-container">
+      <h1 className="book-view-container__heading">My Books</h1>
+      <div className="book-view">
+        <Book
+          title={props.books[0].bookTitle}
+          author={props.books[0].bookAuthor}
+          pages={props.books[0].pages}
+          date={props.books[0].dateCompleted}
+        />
+        <Book
+          title={props.books[1].bookTitle}
+          author={props.books[1].bookAuthor}
+          pages={props.books[1].pages}
+          date={props.books[1].dateCompleted}
+        />
+        <Book
+          title={props.books[2].bookTitle}
+          author={props.books[2].bookAuthor}
+          pages={props.books[2].pages}
+          date={props.books[2].dateCompleted}
+        />
+        <Book
+          title={props.books[3].bookTitle}
+          author={props.books[3].bookAuthor}
+          pages={props.books[3].pages}
+          date={props.books[3].dateCompleted}
+        />
+      </div>
     </div>
   );
 };
