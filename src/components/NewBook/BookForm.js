15b2bf38e4fd20692c92273905e5127115653af5
Nick M <mully7773@gmail.com>
2022-12-02 10:23:14 -0600

    Add state for capturing form values on submit - save all values to object called bookData

--- src/components/NewBook/BookForm.js
+++ src/components/NewBook/BookForm.js
@@ -2,25 +2,44 @@ import React, { useState } from "react";
 import "./BookForm.scss";
 
 const BookForm = (props) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [page, setPage] = useState("");
+  const [date, setDate] = useState("");
+
   const titleChangeHandler = (e) => {
     console.log(e.target.value);
-    // setTitle(e.target.value);
+    setTitle(e.target.value);
   };
 
   const authorChangeHandler = (e) => {
     console.log(e.target.value);
+    setAuthor(e.target.value);
   };
 
   const pageChangeHandler = (e) => {
     console.log(e.target.value);
+    setPage(e.target.value);
   };
 
   const dateChangeHandler = (e) => {
     console.log(e.target.value);
+    setDate(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const bookData = {
+      bookTitle: title,
+      bookAuthor: author,
+      bookPages: page,
+      completionDate: new Date(date + "T00:00:00"),
+    };
+    console.log(bookData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-book__inputs">
         <div className="new-book__input">
           <label>Title</label>
