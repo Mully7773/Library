278ee0482c3c196e2221294d9d5df8c4df8bff25
Nick M <mully7773@gmail.com>
2022-12-02 10:09:54 -0600

    Add handlers to form inputs and successfully logged data to console

--- src/components/NewBook/BookForm.js
+++ src/components/NewBook/BookForm.js
@@ -1,24 +1,50 @@
+import React, { useState } from "react";
 import "./BookForm.scss";
 
-const BookForm = () => {
+const BookForm = (props) => {
+  const titleChangeHandler = (e) => {
+    console.log(e.target.value);
+    // setTitle(e.target.value);
+  };
+
+  const authorChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
+  const pageChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <form>
       <div className="new-book__inputs">
         <div className="new-book__input">
           <label>Title</label>
-          <input placeholder="Ender's Game" type="text" />
+          <input
+            placeholder="Ender's Game"
+            type="text"
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-book__input">
           <label>Author</label>
-          <input placeholder="Orson Scott Card" type="text" />
+          <input
+            placeholder="Orson Scott Card"
+            type="text"
+            onChange={authorChangeHandler}
+          />
         </div>
         <div className="new-book__input">
           <label>Pages</label>
-          <input placeholder="324" type="number" />
+          <input placeholder="324" type="number" onChange={pageChangeHandler} />
         </div>
         <div className="new-book__input">
           <label>Date Completed</label>
-          <input placeholder="Ender's Game" type="date" />
+          <input type="date" onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-book__action">
