410b1f32666ebb52e7465d31e6f030bf6b95ec89
Nick M <mully7773@gmail.com>
2022-12-07 16:03:04 -0600

    Convert hard-coded data to dynamic data in BookView.js for fetching books

--- src/components/UI/BookView.js
+++ src/components/UI/BookView.js
@@ -7,30 +7,14 @@ const BookView = (props) => {
     <div className="book-view-container">
       <h1 className="book-view-container__heading">My Books</h1>
       <div className="book-view">
-        <Book
-          title={props.books[0].bookTitle}
-          author={props.books[0].bookAuthor}
-          pages={props.books[0].pages}
-          date={props.books[0].dateCompleted}
-        />
-        <Book
-          title={props.books[1].bookTitle}
-          author={props.books[1].bookAuthor}
-          pages={props.books[1].pages}
-          date={props.books[1].dateCompleted}
-        />
-        <Book
-          title={props.books[2].bookTitle}
-          author={props.books[2].bookAuthor}
-          pages={props.books[2].pages}
-          date={props.books[2].dateCompleted}
-        />
-        <Book
-          title={props.books[3].bookTitle}
-          author={props.books[3].bookAuthor}
-          pages={props.books[3].pages}
-          date={props.books[3].dateCompleted}
-        />
+        {props.books.map((book) => (
+          <Book
+            title={book.bookTitle}
+            author={book.bookAuthor}
+            pages={book.pages}
+            date={book.dateCompleted}
+          />
+        ))}
       </div>
     </div>
   );
