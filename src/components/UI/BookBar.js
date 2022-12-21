9eb6dac0655f19f1d878b4e9d7cc5d709abc3e06
Nick M <mully7773@gmail.com>
2022-12-21 14:23:00 -0600

    Add dynamically updating date to book bar for current date

--- src/components/UI/BookBar.js
+++ src/components/UI/BookBar.js
@@ -1,13 +1,19 @@
 import "./BookBar.scss";
 
 const BookBar = (props) => {
+  const newDate = new Date();
+  const year = newDate.getFullYear();
+  const month = newDate.toLocaleString("default", { month: "long" });
+  const day = newDate.getDate();
+  const todayString = `pages read as of today, ${month} ${day}, ${year}`;
+
   const heading = "My Books";
-  //   const currentDate = new Date().toLocaleDateString;
+
   return (
     <div className="book-bar">
       <h1 className="book-bar__heading">{heading}</h1>
       <span className="book-bar__page-total">
-        <strong>1256</strong> pages read as of today, July 21
+        <strong>1256</strong> {todayString}
       </span>
     </div>
   );
