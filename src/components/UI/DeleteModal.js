860a2a64c23fc774d17c045451fce06c97b68f9d
Nick M <mully7773@gmail.com>
2023-01-06 14:13:13 -0600

    Add a few more styles to DeleteModal component

--- src/components/UI/DeleteModal.js
+++ src/components/UI/DeleteModal.js
@@ -1,6 +1,8 @@
 import Button from "./Button";
 import "./DeleteModal.scss";
 
+import { ReactComponent as Flourish } from ".././img/Birds-Flourish.svg";
+
 const DeleteModal = (props) => {
   const deleteBook = () => {
     props.setBooks(
@@ -24,6 +26,9 @@ const DeleteModal = (props) => {
             Are you sure you want to delete this book?
           </h2>
         </header>
+        <section>
+          <Flourish alt="bird flourish logo" className="modal-container__img" />
+        </section>
         <footer className="modal-container__footer">
           <Button onClick={deleteBook} className="modal-container__btn">
             Yes
